b355658125520751c80bd20251d6fb7e9aca7833
A. Monae Payne <amonaepayne@gmail.com>
2021-06-13 13:40:25 -0500

    :tada: initial commit
    I got some things set up, and I already got it to
    where the user location is saved in local storage,
    which will be useful when searching for a pet. The
    user won't have to put in their location every time
    after leaving the site and coming back.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [location, setLocation] = useState({});
+  const [selectedSpecies, setSelectedSpecies] = useState("");
+
+  useEffect(() => {
+    let userLocation = {
+      longitude: localStorage.getItem("longitude"),
+      latitude: localStorage.getItem("latitude"),
+    };
+
+    if (!userLocation.longitude || !userLocation.latitude) {
+      navigator.geolocation.getCurrentPosition((pos) => {
+        let coordinates = pos.coords;
+        setLocation({
+          longitude: coordinates.longitude.toString(),
+          latitude: coordinates.latitude.toString(),
+        });
+        window.localStorage.setItem(
+          "longitude",
+          coordinates.longitude.toString()
+        );
+        window.localStorage.setItem(
+          "latitude",
+          coordinates.latitude.toString()
+        );
+      });
+    } else {
+      setLocation(userLocation);
+    }
+  }, []);
+
+  const handleSearch = (e: any) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div>Hello world!</div>
+      <form>
+        <select
+          name="species"
+          id="species"
+          onChange={(e) => setSelectedSpecies(e.currentTarget.value)}
         >
-          Learn React
-        </a>
-      </header>
+          <option value="Dog">Dog</option>
+          <option value="Cat">Cat</option>
+          <option value="Bird">Bird</option>
+          <option value="Rabbit">Rabbit</option>
+          <option value="Horse">Horse</option>
+          <option value="Reptile">Reptile</option>
+        </select>
+        <input type="text" placeholder="Search for a breed" />
+        <button type="submit">Find</button>
+      </form>
     </div>
   );
 }
