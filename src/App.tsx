6aa3934813045eb7d49edc776e81be20a4711eb4
A. Monae Payne <amonaepayne@gmail.com>
2021-06-15 01:13:25 -0500

    :construction: fetch data
    This code needs serious refactoring! It might be
    better for me to abstract the entire "data fetching"
    part of the code into separate functions that can
    go inside of a "utils" folder.
    
    Also, I've decided to store the auth token inside
    of a cookie. Yes, I know that's not a good idea and
    poses huge security issues, but since this is just
    a small personal project, I figured it's
    nbd. But my reason for doing this is because
    PetFinder's expiration date for the token is
    extremely short (like an hour), and I want the
    client to automatically be able to get a new
    token when it expires.
    
    Also, yes, my token refetching implementation needs
    a huge refactor bc it makes zero sense,
    I'm not done with it yet lol.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
+// import axios from "axios";
 import logo from "./logo.svg";
 import "./App.css";
+const axios = require("axios");
+
+interface LocationI {
+  longitude: string | null;
+  latitude: string | null;
+}
 
 function App() {
-  const [location, setLocation] = useState({});
-  const [selectedSpecies, setSelectedSpecies] = useState("");
+  const [location, setLocation] = useState<LocationI>({
+    longitude: null,
+    latitude: null,
+  });
+  // the default is set to Dog because the select element's "default" selection is Dog
+  const [selectedSpecies, setSelectedSpecies] = useState("Dog");
 
   useEffect(() => {
     let userLocation = {
@@ -35,12 +45,52 @@ function App() {
   }, []);
 
   const handleSearch = (e: any) => {
+    let token: string | undefined = document.cookie
+      .split("; ")
+      .find((row) => row.startsWith("token="))!
+      .split("=")[1];
+
     e.preventDefault();
+    axios({
+      method: "get",
+      headers: {
+        Authorization: `Bearer ${process.env.REACT_APP_BEARER_TOKE}`,
+      },
+      url: "https://api.petfinder.com/v2/animals",
+      params: {
+        location: `${parseFloat(location.latitude!).toString()}, ${parseFloat(
+          location.longitude!
+        ).toString()}`,
+      },
+    })
+      .then((data: any) => console.log(data))
+      .catch((err: any) => {
+        let cookie = document.cookie
+          .split(";")
+          .some((item) => item.trim().startsWith("token="));
+        if (!cookie) {
+          axios({
+            method: "post",
+            url: "https://api.petfinder.com/v2/oauth2/token",
+            data: {
+              grant_type: "client_credentials",
+              client_id: process.env.REACT_APP_API_KEY,
+              client_secret: process.env.REACT_APP_API_SECRET,
+            },
+          }).then((data: any) => {
+            let expiry: number = data.data.expires_in;
+            let accessToken: string = data.data.access_token;
+            document.cookie = `token=${accessToken}; SameSite=None; max-age=${
+              expiry - 100
+            }; Secure`;
+          });
+        }
+      });
   };
 
   return (
     <div className="App">
-      <div>Hello world!</div>
+      <div>Currently selected species: {selectedSpecies} </div>
       <form>
         <select
           name="species"
@@ -55,7 +105,9 @@ function App() {
           <option value="Reptile">Reptile</option>
         </select>
         <input type="text" placeholder="Search for a breed" />
-        <button type="submit">Find</button>
+        <button type="submit" onClick={(e) => handleSearch(e)}>
+          Find
+        </button>
       </form>
     </div>
   );
