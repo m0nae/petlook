db5073a0bdf44cc773fe2cfa8a0ea75e92c5b500
A. Monae Payne <amonaepayne@gmail.com>
2021-06-23 10:39:55 -0500

    :label: add types for option component

---
+++ src/components/Option/option.types.ts
@@ -0,0 +1,82 @@
+import PropTypes, { InferProps } from "prop-types";
+import {
+  species,
+  size,
+  gender,
+  age,
+  goodWith,
+  environment,
+} from "../../utils/parseSearchOptions";
+
+export namespace SearchOptionTypes {
+  /**
+   * @property foo Some docs.
+   */
+  export type SPECIES =
+    | "dog"
+    | "cat"
+    | "rabbit"
+    | "small & furry"
+    | "horse"
+    | "bird"
+    | "scales, fins, & other"
+    | "barnyard";
+
+  export type SIZE = "small" | "large";
+  export type GENDER = "male" | "female";
+  export type AGE = "young" | "adult" | "old";
+  export type GOOD_WITH = "children" | "dogs" | "cats";
+  export type ENVIRONMENT = "housetrained" | "declawed" | "special-needs";
+  export type ALL_OPTIONS =
+    | "species"
+    | "size"
+    | "gender"
+    | "age"
+    | "goodWith"
+    | "environment";
+  export type ALL_OPTIONS_TYPE =
+    | SIZE
+    | GENDER
+    | AGE
+    | SPECIES
+    | GOOD_WITH
+    | ENVIRONMENT;
+}
+
+export type OptionProps = {
+  searchType: string;
+  label: string;
+  option: {
+    optionName:
+      | SearchOptionTypes.SPECIES
+      | SearchOptionTypes.SIZE
+      | SearchOptionTypes.GENDER
+      | SearchOptionTypes.AGE
+      | SearchOptionTypes.GOOD_WITH
+      | SearchOptionTypes.ENVIRONMENT;
+    value: string | (() => string);
+  };
+};
+
+export const propTypes = {
+  searchType: PropTypes.oneOf<SearchOptionTypes.ALL_OPTIONS>([
+    "species",
+    "size",
+    "gender",
+    "age",
+    "goodWith",
+    "environment",
+  ]).isRequired,
+  label: PropTypes.shape({
+    optionName: PropTypes.oneOf<
+      | SearchOptionTypes.SPECIES
+      | SearchOptionTypes.SIZE
+      | SearchOptionTypes.GENDER
+      | SearchOptionTypes.AGE
+      | SearchOptionTypes.GOOD_WITH
+      | SearchOptionTypes.ENVIRONMENT
+    >([...species, ...size, ...gender, ...age, ...goodWith, ...environment])
+      .isRequired,
+    value: PropTypes.oneOfType([PropTypes.string, PropTypes.func]).isRequired,
+  }),
+};
