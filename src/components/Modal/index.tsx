4ff8f2b1a6e616494a19d38635181fdeca23886d
A. Monae Payne <amonaepayne@gmail.com>
2021-06-28 16:13:08 -0500

    :sparkles: display info of pet inside modal
    When the modal opens, display the info of the pet with the ID of the
    selected card. The styles are extremely bare for now, I just needed to
    display some information to have some actual data inside the modal and
    make sure it's "logically" running smoothly.
    
    Clicking either the semi-transparent overlay OR the modal's close
    button will close the modal.

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,10 +1,19 @@
+import { useState, useEffect } from "react";
 import ReactDom from "react-dom";
 import PropTypes, { InferProps } from "prop-types";
-
 export default function Modal({
   isOpen,
   setIsOpen,
+  selectedPetId,
+  data,
 }: InferProps<typeof Modal.propTypes>) {
+  const [currentPet, setCurrentPet] = useState<any>({});
+  let { name, species, age, gender, photos } = currentPet[0] ?? {};
+
+  useEffect(() => {
+    setCurrentPet(data.animals.filter((animal) => animal.id === selectedPetId));
+  }, [selectedPetId, data]);
+
   if (!isOpen) return null;
   return ReactDom.createPortal(
     <>
@@ -17,8 +26,25 @@ export default function Modal({
         id="modal"
         className="transform -translate-x-1/2 -translate-y-1/2 z-[100] fixed top-[50%] left-[50%] h-[400px] w-[550px] bg-white rounded-xl"
       >
-        <p>Some content here.</p>
-        <button onClick={() => setIsOpen(false)}>Close modal</button>
+        <div id="modal-container" className="flex flex-col m-3">
+          <img
+            src={
+              photos &&
+              (photos[0]
+                ? photos[0].large
+                : "https://via.placeholder.com/480x325")
+            }
+            alt="Pet"
+            className="object-cover max-h-[200px] rounded-md"
+          />
+          <div id="modal-pet-info" className="">
+            <h2 className="text-2xl font-bold">{name}</h2>
+            <p>{species && species}</p>
+            <p>{age && age}</p>
+            <p>{gender && gender}</p>
+            <button onClick={() => setIsOpen(false)}>Close modal</button>
+          </div>
+        </div>
       </div>
     </>,
     document.getElementById("modal")!
@@ -28,4 +54,8 @@ export default function Modal({
 Modal.propTypes = {
   isOpen: PropTypes.bool.isRequired,
   setIsOpen: PropTypes.func.isRequired,
+  selectedPetId: PropTypes.number,
+  data: PropTypes.shape({
+    animals: PropTypes.array.isRequired,
+  }).isRequired,
 };
