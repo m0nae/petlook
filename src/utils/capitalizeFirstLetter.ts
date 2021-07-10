9ce34d4d358730918807ad4f6d0643e1486edf4d
A. Monae Payne <amonaepayne@gmail.com>
2021-07-09 21:21:24 -0500

    :recycle: remove "if" in capitalizeFirstLetter fn
    It's redundant IMO and was clashing with Typescript. Typescript is for
    type checking, not IF statements like that!

--- src/utils/capitalizeFirstLetter.ts
+++ src/utils/capitalizeFirstLetter.ts
@@ -1,15 +1,11 @@
 export function capitalizeFirstLetter(str: string) {
-  if (typeof str !== "string") {
-    return;
-  } else {
-    let words = str.split(" ");
-    let newWords = words.map((word) => {
-      let letters = word.split("");
-      let firstLetter = letters[0];
-      firstLetter = firstLetter.toUpperCase();
+  let words = str.split(" ");
+  let newWords = words.map((word) => {
+    let letters = word.split("");
+    let firstLetter = letters[0];
+    firstLetter = firstLetter.toUpperCase();
 
-      return firstLetter.concat(letters.splice(1).join(""));
-    });
-    return newWords.join(" ");
-  }
+    return firstLetter.concat(letters.splice(1).join(""));
+  });
+  return newWords.join(" ");
 }
