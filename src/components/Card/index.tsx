e482e745020252c5263373f18bfff3ec18e7b21f
A. Monae Payne <amonaepayne@gmail.com>
2021-07-11 02:25:56 -0500

    :lipstick::recycle: tweak style and comment out unusued logic
    Since there's no modal being used as of now, I commented out the logic
    that renders a modal when a Card is clicked.

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -17,10 +17,10 @@ export default function Card({
     };
   }
 
-  const handleClick = () => {
-    setSelectedPetId(info.id);
-    setIsOpen(true);
-  };
+  // const handleClick = () => {
+  //   setSelectedPetId(info.id);
+  //   setIsOpen(true);
+  // };
 
   return (
     <>
@@ -29,7 +29,7 @@ export default function Card({
         target="_blank"
         rel="noopener noreferrer"
         className="p-[1px] hover:cursor-pointer"
-        onClick={() => handleClick()}
+        // onClick={() => handleClick()}
       >
         <div className="w-[100%] mobile:w-[100%] lg:max-w-sm xl:max-w-lg rounded-xl overflow-hidden border-2 border-gray-100">
           <img
@@ -39,7 +39,7 @@ export default function Card({
           />
           <div
             id="pet-info-container"
-            className="relative px-2 py-2 mobile:px-4 sm:py-4 text-left h-[100px] mobile:h-[125px] md:h-[135px] lg:h-[175px] xl:h-[130px]"
+            className="relative px-2 py-2 mobile:px-4 sm:py-4 text-left h-[100px] mobile:h-[110px] md:h-[135px] lg:h-[175px] xl:h-[130px]"
           >
             <p className="text-gray-700 text-xl text-center mobile:text-left font-semibold md:mb-2 overflow-hidden overflow-ellipsis whitespace-nowrap">
               {name}
