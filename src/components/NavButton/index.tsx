e88c56bbe2a91664f7ffc0658228062958bc8a23
A. Monae Payne <amonaepayne@gmail.com>
2021-07-27 12:55:26 -0500

    :wheelchair: make nav btns accessible

--- src/components/NavButton/index.tsx
+++ src/components/NavButton/index.tsx
@@ -16,8 +16,9 @@ export default function NavButton({
   if (disabled) {
     return (
       <button
+        tabIndex={0}
         className="px-10 mobile:px-14 sm:px-20 py-4 hover:cursor-default bg-gray-400 font-semibold text-white text-2xl rounded-md"
-        disabled
+        aria-disabled
       >
         <span className="hidden mobile:block">{children}</span>
         <span className="mobile:hidden">
@@ -61,45 +62,44 @@ export default function NavButton({
   return (
     <Link
       to={to}
+      role="button"
       className="px-10 mobile:px-14 sm:px-20 py-4 bg-blue-500 font-semibold text-white text-2xl rounded-md"
     >
-      <button>
-        <span className="hidden mobile:block">{children}</span>
-        <span className="mobile:hidden">
-          {direction === "back" && (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-10 w-10"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <title>Left Pointing Arrow</title>
-              <path
-                fill-rule="evenodd"
-                d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z"
-                clip-rule="evenodd"
-              />
-            </svg>
-          )}
-          {direction === "forward" && (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-10 w-10"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <title>Right Pointing Arrow</title>
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M14 5l7 7m0 0l-7 7m7-7H3"
-              />
-            </svg>
-          )}
-        </span>
-      </button>
+      <span className="hidden mobile:block">{children}</span>
+      <span className="mobile:hidden">
+        {direction === "back" && (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-10 w-10"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <title>Left Pointing Arrow</title>
+            <path
+              fill-rule="evenodd"
+              d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        )}
+        {direction === "forward" && (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-10 w-10"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <title>Right Pointing Arrow</title>
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M14 5l7 7m0 0l-7 7m7-7H3"
+            />
+          </svg>
+        )}
+      </span>
     </Link>
   );
 }
